dc687b2142ea3ff178bd5a69f05015e24aeaf6d6
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-11-06 17:19:09 +0300

    create action for open and close list

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 const menu = document.querySelector("#icon-menu");
-const tList = document.getElementsByClassName("toggle-list");
+const closeMenu = document.querySelector("#close-menu");
+const tList = document.getElementsByClassName("toggle-list")[0];
 
 menu.addEventListener("click", ()=>{
+    if(tList.style.display === "none" || tList.style.display ===""){
+        tList.style.display = "block";
+    }
+})
+
+closeMenu.addEventListener("click", ()=>{
+    if(tList.style.display !== "none" && tList.style.display !==""){
+        tList.style.display = "none";
+    }
 })
 
