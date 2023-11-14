f9bf4c840dbb4c197a914368fd55b8903a0f049e
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-11-15 01:04:14 +0300

    Set overlay to spawn when list is un-collapsed

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 const menu = document.querySelector("#icon-menu");
 const closeMenu = document.querySelector("#close-menu");
 const tList = document.getElementsByClassName("toggle-list")[0];
+const bgFilter = document.getElementById("overlay");
 
 menu.addEventListener("click", ()=>{
     if(tList.style.display === "none" || tList.style.display ===""){
         tList.style.display = "block";
+        bgFilter.style.display ="block";
     }
 })
 
 closeMenu.addEventListener("click", ()=>{
     if(tList.style.display !== "none" && tList.style.display !==""){
         tList.style.display = "none";
+        bgFilter.style.display= "none";
     }
 })
 
